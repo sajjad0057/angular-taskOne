98e3069a8cf1b46f6563a1986294b8b05eb3f5ae
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-14 13:38:52 +0600

    update code

--- NgPractice/src/app/app.module.ts
+++ NgPractice/src/app/app.module.ts
@@ -4,40 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { Component1Component } from './dashboard/components/component1/component1.component';
-import { Component2Component } from './dashboard/components/component2/component2.component';
-import { Component3Component } from './dashboard/components/component3/component3.component';
-import { Component4Component } from './dashboard/components/component4/component4.component';
-import { Component5Component } from './dashboard/components/component5/component5.component';
-import { Component6Component } from './dashboard/components/component6/component6.component';
-import { HomeComponent } from './dashboard/components/home/home.component';
-import { InvalidComponent } from './dashboard/components/invalid/invalid.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent,
-    Component1Component,
-    Component2Component,
-    Component3Component,
-    Component4Component,
-    Component5Component,
-    Component6Component,
-    HomeComponent,
-    InvalidComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- NgPractice/src/app/dashboard/dashboard.module.ts
+++ NgPractice/src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
+import { Component4Component } from './components/component4/component4.component';
+import { Component5Component } from './components/component5/component5.component';
+import { Component6Component } from './components/component6/component6.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component,
+    Component5Component,
+    Component6Component,
+    DashboardComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
+  ],
+  exports:[
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
